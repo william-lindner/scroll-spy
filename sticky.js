d3414a8fe0e2e0eec87d22d1b85cfbc954d3b50f
William Lindner <38209603+william-lindner@users.noreply.github.com>
2019-10-18 13:08:34 -0400

    Create sticky.js

---
+++ sticky.js
@@ -0,0 +1,52 @@
+class Sticky {
+    constructor (el, className = 'is-sticky') {
+        if (!(el instanceof HTMLElement)) {
+            console.error('[Bizjournals] Invalid sticky implementation. Must pass element as first argument.');
+            return;
+        }
+
+        this.$el = el;
+        this.$className = className;
+        this.$rootElement = document.documentElement;
+        this.$top = 0;
+
+        document.onscroll = () => this.position().check();
+        this.position().check();
+    }
+
+    check () {
+        if (this.$rootElement.scrollTop > this.$top) {
+            this.$el.classList.add(this.$className);
+        } else {
+            this.$el.classList.remove(this.$className);
+        }
+
+        return this;
+    }
+
+    position () {
+        if (this.$el.classList.contains(this.$className)) {
+            return this;
+        }
+
+        let newDocStart = this.$rootElement.scrollTop,
+            newClientRel = this.$el.getBoundingClientRect(),
+            newClientTop = newClientRel.y || newClientRel.top,
+            newTop = newClientTop + newDocStart - 15;
+
+        newTop = newTop < 0 ? 0 : newTop;
+
+        if (this.$top === newTop) {
+            return this;
+        }
+
+        this.$top = newTop;
+        return this;
+    }
+
+}
+
+
+export default function (...args) {
+    return new Sticky(...args);
+}
